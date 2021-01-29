a68c0cfbbcd7fd63b2d1697c288e6dc2a3f59da8
Eze Ochonogor <sendezeamail@gmail.com>
2021-01-29 14:41:04 +0100

    VW

--- src/components/profiles.js
+++ src/components/profiles.js
@@ -5,7 +5,7 @@ export default function Profiles({currentProfiles}) {
     return (
         <div className="profiles grid_display"> 
          {currentProfiles.length > 0 && currentProfiles.map(eachProfile =>
-             <SingleProfile key={eachProfile.email} eachProfile={eachProfile} />
+             <SingleProfile key={eachProfile.CreditCardNumber} eachProfile={eachProfile} />
               )}
         </div>
     )
